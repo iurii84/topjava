ac9f99ea72171eeebb8b274d8fd74704c1031804
Iuri Ostrikov <iurii84@gmail.com>
2019-06-30 16:18:56 -0400

    JPA entity MEAL JPA repository MEAL patch @UniqueConstraint

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -21,7 +21,7 @@ import java.time.LocalTime;
 })
 
 @Entity
-@Table(name = "meals")
+@Table(name = "meals", uniqueConstraints = {@UniqueConstraint(columnNames = {"user_id", "date_time"})})
 public class Meal extends AbstractBaseEntity {
 
     public static final String ALL_SORTED = "Meal.getAll";
