82fc89d2932c3c13ca64f1075e40b1a256982d82
Iuri Ostrikov <iurii84@gmail.com>
2019-08-19 18:50:05 -0400

    return HttpStatus.UNPROCESSABLE_ENTITY

--- src/main/java/ru/javawebinar/topjava/util/exception/ValidationFieldsException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/ValidationFieldsException.java
@@ -1,13 +1,17 @@
 package ru.javawebinar.topjava.util.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 public class ValidationFieldsException extends RuntimeException {
+
     public ValidationFieldsException(BindingResult bindingResult) {
         super(getDescription(bindingResult));
 
     }
 
+    @ResponseStatus(value = HttpStatus.UNPROCESSABLE_ENTITY)
     private static String getDescription(BindingResult bindingResult) {
         StringBuilder stringBuilder = new StringBuilder();
         bindingResult.getFieldErrors().forEach(
